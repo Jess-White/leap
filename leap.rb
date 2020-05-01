64fed06bfd0fb9f68534640dc0e63540cf472f9f
jesswhite.chicago@gmail.com <58121322+Jess-White@users.noreply.github.com>
2020-04-30 21:33:26 -0500

    first commit

---
+++ leap.rb
@@ -0,0 +1,20 @@
+class Year
+
+  def self.leap?(year)
+    if year % 400 == 0
+      return true
+    elsif year % 100 == 0
+      return false
+    elsif year % 4 == 0
+      return true
+    else
+      return false
+    end 
+  end 
+
+end 
+
+
+# on every year that is evenly divisible by 4
+#   except every year that is evenly divisible by 100
+#     unless the year is also evenly divisible by 400
